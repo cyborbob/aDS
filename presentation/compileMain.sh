91c50e6664528d12b19650662c8d5f36b547b4fd
Roland <anniel12@student.aau.dk>
2014-10-30 14:13:24 +0100

    Created a stub for the presentation

--- presentation/compileMain.sh
+++ presentation/compileMain.sh
@@ -3,3 +3,9 @@ pdflatex -halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex
 bibtex $REPORT_MAIN 
 pdflatex --halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex 
 pdflatex --halt-on-error -interaction nonstopmode $REPORT_MAIN.tex 
+rm *.log
+rm *.nav
+rm *.out
+rm *.aux
+rm *.snm
+rm *.toc
